ce2f0503f61a95618fb07a9c3bc54ef90e392e19
Diego Lattanzio <lattanzio.diego@gmail.com>
2023-04-23 20:45:29 -0300

    first commit

---
+++ src/test/java/com/example/springversiontest/SpringVersionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springversiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVersionTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
